444065f3619063587018f3bad9b165ce429dad1b
ChameleonChen <812757854@qq.com>
2014-07-19 17:31:20 +0800

    实现JFinalClassLoader的构造函数
    没还有弄明白ClassLoader的机制。

---
+++ src/org/myjfinal/server/JFinalClassLoader.java
@@ -0,0 +1,39 @@
+package org.myjfinal.server;
+
+import java.io.File;
+import java.io.IOException;
+import org.eclipse.jetty.webapp.WebAppClassLoader;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class JFinalClassLoader extends WebAppClassLoader {
+	private boolean initialized = false;
+	
+	public JFinalClassLoader(WebAppContext context, String classPath) throws IOException {
+		super(context);
+		
+		/*
+		 * classPath = System.getProperty("java.class.path");
+		 * getProperty()将许多条classPath路径以File.pathSeparatorChar (Windows中为 ; )相隔
+		 * 的方式连接成一条字符串。
+		 * 如：                                                                                                                                                                                                                                                                  |间隔在此
+		 *   classPath == C:\Users\cqr\Documents\GitHub\JFinalLearn\WebRoot\WEB-INF\classes;C:\Users\cqr\Documents\GitHub\JFinalLearn\WebRoot\WEB-INF\lib\jfinal-1.8-bin-with-src.jar;C:\Users\cqr\Documents\GitHub\JFinalLearn\WebRoot\WEB-INF\lib\jetty-server-8.1.8.jar;C:\Users\cqr\Documents\GitHub\JFinalLearn\WebRoot\WEB-INFlib\c3p0-0.8.4.5.jar
+		 * 将classPath进行分割得到String[] tokens，最后遍历为WebAppClassLoader添加路径。
+		 */
+		if (classPath != null) {
+			String[] tokens = classPath.split(String.valueOf(File.pathSeparatorChar));
+			for (String entry : tokens) {
+				String path = entry;
+				if(path.startsWith("-y-") || path.startsWith("-n-")) {
+					path = path.substring(3);
+				}
+				
+				if(entry.startsWith("-n-") == false){
+					super.addClassPath(path);
+				}
+			}
+		}
+		
+		initialized = true;
+	}
+	
+}
