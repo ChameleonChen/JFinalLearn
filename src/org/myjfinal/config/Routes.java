9400d27c1dfc97e8e67e1dd70b0937c58798559e
ChameleonChen <812757854@qq.com>
2014-07-28 15:45:09 +0800

    构建Routes：完成路由添加功能
    Statement：
    路由由两个HashMap进行管理，一个管理Controller，另一个管理viewPath。
    
    Finished：
    完成路由添加的add方法，具有多态。
    未测试。
    
    TODO：
    实现baseViewPath的配置调用过程。

--- src/org/myjfinal/config/Routes.java
+++ src/org/myjfinal/config/Routes.java
@@ -2,6 +2,7 @@ package org.myjfinal.config;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.myjfinal.core.Controller;;
 
 public abstract class Routes {
@@ -9,12 +10,31 @@ public abstract class Routes {
 	private final Map<String, Class<? extends Controller>> map = new HashMap<String, Class<? extends Controller>>();
 	private final Map<String, String> viewPathMap = new HashMap<String, String>();
 	
+	private static String baseViewPath;
 	
 	public abstract void config();
 	
+	public static void setBaseViewPath(String baseViewPath) {
+		if (baseViewPath == null)
+			throw new NullPointerException("the baseViewPath can not be null");
+		if ("".equals(baseViewPath)) {
+			throw new IllegalArgumentException("the baseViewPath can not be blank");
+		}
+		
+		baseViewPath = baseViewPath.trim();
+		if (!baseViewPath.startsWith("/")) {
+			baseViewPath = "/" + baseViewPath;
+		}
+		if (baseViewPath.endsWith("/")) {
+			baseViewPath = baseViewPath.substring(0, baseViewPath.length() - 1);
+		}
+		
+		Routes.baseViewPath = baseViewPath;
+	}
+	
 	public Routes add(Routes routes) {
 		if (routes != null) {
-			this.config();    //TODO 弄清楚到底config什么。
+			this.config();    //TODO 搞清config()作用
 			this.map.putAll(routes.map);
 			this.viewPathMap.putAll(routes.viewPathMap);
 		}
@@ -22,6 +42,44 @@ public abstract class Routes {
 	}
 	
 	public Routes add(String controllerKey, Class<? extends Controller> controller, String viewPath) {
+		if (controllerKey == null)
+			throw new NullPointerException("the controllerKey can not be null");
+		if ("".equals(controllerKey))
+			throw new IllegalArgumentException("the controllerKey can not be blank");
+		if (controller == null)
+			throw new NullPointerException("the controller can not be null");
 		
+		if (!controllerKey.startsWith("/")) {
+			controllerKey = "/" + controllerKey;
+		}
+		
+		if (viewPath == null || "".equals(viewPath.trim())) {
+			viewPath = controllerKey;
+		}else {
+			viewPath = viewPath.trim();
+			if (!viewPath.startsWith("/")) {
+				viewPath = "/" + viewPath;
+			}
+			if (!viewPath.endsWith("/")) {
+				viewPath = viewPath + "/";
+			}
+			
+			if (baseViewPath != null) {
+				viewPath = baseViewPath + viewPath;
+			}
+		}
+		
+		if (!map.containsKey(controllerKey)) {
+			map.put(controllerKey, controller);
+		}
+		if (!viewPathMap.containsKey(viewPath)) {
+			viewPathMap.put(controllerKey, viewPath);
+		}
+		
+		return this;
+	}
+	
+	public Routes add(String controllerKey, Class<? extends Controller> controller) {
+		return add(controllerKey, controller, controllerKey);
 	}
 }
