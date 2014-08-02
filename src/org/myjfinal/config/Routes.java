14ed0ec9446e08e27439162904aa2a71d9ac3fad
ChameleonChen <812757854@qq.com>
2014-08-02 18:12:10 +0800

    构建Routes类：完成Routes类

--- src/org/myjfinal/config/Routes.java
+++ src/org/myjfinal/config/Routes.java
@@ -2,9 +2,18 @@ package org.myjfinal.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.myjfinal.core.Controller;;
 
+/**
+ * Routes拥有两个分别管理|Controller|和viewPath的|HashMap|,
+ * 默认情况下，两个表拥有相同的key，这使得controller和viewPath形成简介的映射关系。
+ * 
+ * @author ChameleonChen
+ *
+ */
 public abstract class Routes {
 	
 	private final Map<String, Class<? extends Controller>> map = new HashMap<String, Class<? extends Controller>>();
@@ -41,6 +50,20 @@ public abstract class Routes {
 		return this;
 	}
 	
+	/**
+	 *   web请求时，通过controllerKey可以访问controller,
+	 * 例如：
+	 *     controllerKey         -->    controller.index()
+	 *     controllerKey/method  -->    controller.method()
+	 *   
+	 *   controller需要对应一个viewPath，此两个分别保存在以controllerKey为
+	 * key的HashMap中。
+	 * 
+	 * @param controllerKey
+	 * @param controller
+	 * @param viewPath
+	 * @return
+	 */
 	public Routes add(String controllerKey, Class<? extends Controller> controller, String viewPath) {
 		if (controllerKey == null)
 			throw new NullPointerException("the controllerKey can not be null");
@@ -81,5 +104,13 @@ public abstract class Routes {
 	
 	public Routes add(String controllerKey, Class<? extends Controller> controller) {
 		return add(controllerKey, controller, controllerKey);
+	}
+	
+	public Set<Entry<String, Class<? extends Controller>>> getControllerEntrySet() {
+		return map.entrySet();
+	}
+	
+	public String getViewPath(String viewPathKey) {
+		return viewPathMap.get(viewPathKey);
 	}
 }
